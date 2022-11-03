dface477a7fe0f6350890284f014ab386ed526cf
Alin Panaitiu <alin.panaitiu@gmail.com>
2022-11-03 19:00:59 +0200

    Convenience functions

--- Sources/FuzzyMatcher/fuzzy-matcher.swift
+++ Sources/FuzzyMatcher/fuzzy-matcher.swift
@@ -1,10 +1,19 @@
 import RustXcframework
-public func fuzzy_find<GenericToRustStr: ToRustStr>(_ pattern: GenericToRustStr, _ lines: GenericToRustStr) -> Optional<RustString> {
-    return lines.toRustStr({ linesAsRustStr in
-        return pattern.toRustStr({ patternAsRustStr in
-        { let val = __swift_bridge__$fuzzy_find(patternAsRustStr, linesAsRustStr); if val != nil { return RustString(ptr: val!) } else { return nil } }()
-    })
-    })
+
+public extension Array where Element == String {
+    func fuzzyFind<GenericToRustStr: ToRustStr>(_ pattern: GenericToRustStr) -> String? {
+        FuzzyMatcher.fuzzyFind(pattern, in: self)
+    }
 }
 
+public func fuzzyFind<GenericToRustStr: ToRustStr>(_ pattern: GenericToRustStr, in lines: [String]) -> String? {
+    fuzzy_find(pattern, lines.joined(separator: "§") as! GenericToRustStr)?.toString()
+}
 
+func fuzzy_find<GenericToRustStr: ToRustStr>(_ pattern: GenericToRustStr, _ lines: GenericToRustStr) -> RustString? {
+    return lines.toRustStr { linesAsRustStr in
+        pattern.toRustStr { patternAsRustStr in
+            { let val = __swift_bridge__$fuzzy_find(patternAsRustStr, linesAsRustStr); if val != nil { return RustString(ptr: val!) } else { return nil } }()
+        }
+    }
+}
